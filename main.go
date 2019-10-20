eae50db8887f01f355055776b9762e3a46f3a20c
BBerastegui <bberastegui@users.noreply.github.com>
2019-10-20 20:05:11 +0100

    First commit

---
+++ main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"net"
+	"os"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/miekg/dns"
+)
+
+func main() {
+
+	servers := []string{
+		"8.8.8.8",
+		"8.8.4.4",
+		"9.9.9.9",
+		"1.1.1.1",
+		"1.0.0.1",
+	}
+
+	rand.Seed(time.Now().Unix())
+
+	type job struct{ domain, server string }
+	jobs := make(chan job)
+
+	var wg sync.WaitGroup
+	for i := 0; i < 20; i++ {
+		wg.Add(1)
+
+		go func() {
+			for j := range jobs {
+
+				cname, err := getCNAME(j.domain, j.server)
+				if err != nil {
+					//fmt.Println(err)
+					continue
+				}
+
+				fmt.Println(cname)
+			}
+			wg.Done()
+		}()
+	}
+
+	sc := bufio.NewScanner(os.Stdin)
+
+	for sc.Scan() {
+		target := strings.ToLower(strings.TrimSpace(sc.Text()))
+		if target == "" {
+			continue
+		}
+		server := servers[rand.Intn(len(servers))]
+
+		jobs <- job{target, server}
+	}
+	close(jobs)
+
+	wg.Wait()
+
+}
+
+func resolves(domain string) bool {
+	_, err := net.LookupHost(domain)
+	return err == nil
+}
+
+func getCNAME(domain, server string) (string, error) {
+	c := dns.Client{}
+
+	m := dns.Msg{}
+	if domain[len(domain)-1:] != "." {
+		domain += "."
+	}
+	m.SetQuestion(domain, dns.TypeCNAME)
+	m.RecursionDesired = true
+
+	r, _, err := c.Exchange(&m, server+":53")
+	if err != nil {
+		return "", err
+	}
+
+	if len(r.Answer) == 0 {
+		return "", fmt.Errorf("no answers for %s", domain)
+	}
+
+	for _, ans := range r.Answer {
+		if r, ok := ans.(*dns.CNAME); ok {
+			return r.Target, nil
+		}
+	}
+	return "", fmt.Errorf("no cname for %s", domain)
+
+}
